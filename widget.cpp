94dc72e0ada8ebdb04832b9c8c97088669118954
Yuno <prcups@v-signal.xyz>
2022-02-10 21:46:49 +0800

    bullets work

--- widget.cpp
+++ widget.cpp
@@ -1,51 +1,80 @@
 #include "widget.h"
 
+//typedef (*nodeType)[ELEMENT_WIDTH_NUM] nodeMatrix;
+
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
 {
     this->setWindowTitle("飞机大战");
-    this->setFixedSize(400, 400);
+    this->setFixedSize(ELEMENT_HEIGHT_NUM * ELEMENT_EDGE_LENGTH, ELEMENT_WIDTH_NUM * ELEMENT_EDGE_LENGTH);
+    mem = new QSharedMemory("mem");
+    mem->create(ELEMENT_HEIGHT_NUM * ELEMENT_WIDTH_NUM * sizeof(nodeType));
+    memdata = static_cast<nodeType (*)[ELEMENT_WIDTH_NUM]>(mem->data());
+    mem->lock();
+    memset(memdata, 0, sizeof((nodeType **) memdata));
+    mem->unlock();
 
-    m_draw_intr_device = new QTimer(this);
+    myPlaneImage = new QImage(":/MyPlane.ico");
+    evilPlaneImage = new QImage(":/EvilPlane.ico");
+
     m_clock_intr_device = new QTimer(this);
+    myp = new MyPlane(24, 12, MyPlaneT);
+    evilp = new EvilPlane(0, 13, EvilPlaneT);
 
-    connect(m_draw_intr_device, &QTimer::timeout, this, QOverload<>::of(&Widget::update));
     connect(m_clock_intr_device, &QTimer::timeout, this, &Widget::ClockIntrExec);
+    connect(evilp, &Plane::statusChanged, this, &Widget::handleStatus);
+    connect(myp, &Plane::statusChanged, this, &Widget::handleStatus);
 
-    m_draw_intr_device->start(40);
-    m_clock_intr_device->start(400);
+    m_clock_intr_device->start(500);
 }
 
 Widget::~Widget() {
-    delete m_draw_intr_device;
+    delete evilp;
+    delete myp;
     delete m_clock_intr_device;
-}
-
-void Widget::keyPressEvent(QKeyEvent *e){
-
+    delete mem;
+    delete myPlaneImage;
+    delete evilPlaneImage;
 }
 
 void Widget::paintEvent(QPaintEvent *e) {
     painter.begin(this);
     painter.setBrush(Qt::SolidPattern);
-    memLock.lockForRead();
-    for (int i = 0; i < 25; ++i) {
-        for (int j = 0; j < 25; ++j) {
-            if (mem[i][j] == 1) {
-                painter.drawText(i * 16, j * 16, "\u2708");
+    for (int i = 0; i < ELEMENT_HEIGHT_NUM; ++i) {
+        for (int j = 0; j < ELEMENT_WIDTH_NUM; ++j) {
+            switch (memdata[i][j]) {
+            case MyPlaneT:
+                painter.drawImage(QRect{j * ELEMENT_EDGE_LENGTH, i * ELEMENT_EDGE_LENGTH,
+                                        ELEMENT_EDGE_LENGTH, ELEMENT_EDGE_LENGTH},
+                                  *myPlaneImage);
+                break;
+            case EvilPlaneT:
+                painter.drawImage(QRect{j * ELEMENT_EDGE_LENGTH, i * ELEMENT_EDGE_LENGTH,
+                                        ELEMENT_EDGE_LENGTH, ELEMENT_EDGE_LENGTH},
+                                  *evilPlaneImage);
+                break;
+            case MyBulletT:
+            case EvilBulletT:
+                painter.drawRect(j * ELEMENT_EDGE_LENGTH, i * ELEMENT_EDGE_LENGTH,
+                                 ELEMENT_EDGE_LENGTH, ELEMENT_EDGE_LENGTH);
+                break;
+            case Empty:
+                break;
             }
+
         }
     }
-    memLock.unlock();
     painter.end();
 }
 
 void Widget::ClockIntrExec() {
-    memLock.lockForWrite();
-    for (int i = 0; i < 25; ++i) {
-        for (int j = 0; j < 25; ++j) {
-            mem[i][j] = random.bounded(2);
-        }
+    myp->start();
+    evilp->start();
+    this->update();
+}
+
+void Widget::handleStatus(statusType status) {
+    if (status != Running) {
+        m_clock_intr_device->stop();
     }
-    memLock.unlock();
 }
--- widget.h
+++ widget.h
@@ -10,23 +10,145 @@
 #include <QPaintEvent>
 #include <QRandomGenerator>
 #include <QReadWriteLock>
+#include <QPushButton>
+#include <QLabel>
+#include <QList>
+#include <QImage>
+#include <QSharedMemory>
+#include <QRect>
+#include <QDebug>
+
+#define ELEMENT_HEIGHT_NUM 25
+#define ELEMENT_WIDTH_NUM 25
+#define ELEMENT_EDGE_LENGTH 32
+
+enum nodeType {
+    Empty = 0,
+    MyPlaneT,
+    EvilPlaneT,
+    MyBulletT,
+    EvilBulletT
+};
+
+enum statusType {
+    NotStart = 0,
+    Running,
+    Victory,
+    Failure
+};
+
+struct node {
+    int x, y;
+};
+
+class Plane: public QThread {
+    Q_OBJECT
+
+    statusType m_status;
+protected:
+    QSharedMemory *mem;
+    nodeType (*memdata)[ELEMENT_WIDTH_NUM];
+    QList<node> bullets;
+    nodeType myType;
+    node position;
+public:
+    Plane(int x, int y, nodeType t): m_status(NotStart), mem(new QSharedMemory("mem")), position({x, y}), myType(t) {
+        mem->attach();
+        memdata = (nodeType (*)[ELEMENT_WIDTH_NUM]) mem->data();
+        mem->lock();
+        memdata[position.x][position.y] = myType;
+        mem->unlock();
+    }
+    ~Plane() {
+        delete mem;
+    }
+signals:
+    void statusChanged(statusType status);
+public slots:
+    void setStatus(statusType newStatus) {
+        if (newStatus != m_status) {
+            m_status = newStatus;
+            emit statusChanged(newStatus);
+        }
+    }
+};
+
+class MyPlane: public Plane {
+    using Plane::Plane;
+    int ifEmit = 0;
+public:
+    virtual void run() {
+        if ((++ifEmit) % 2) {
+            bullets.insert(bullets.begin(), {position.x, position.y});
+        }
+        for (QList<node>::iterator i = bullets.begin(); i != bullets.end();) {
+            if (i->x) {
+                --i->x;
+                mem->lock();
+                if (memdata[i->x + 1][i->y] == EvilBulletT) memdata[i->x + 1][i->y] = Empty;
+                if (memdata[i->x][i->y] == EvilPlaneT) setStatus(Victory);
+                else if (memdata[i->x][i->y == MyPlaneT]) setStatus(Failure);
+                else memdata[i->x][i->y] = EvilBulletT;
+                mem->unlock();
+                ++i;
+            } else {
+                mem->lock();
+                memdata[i->x][i->y] = Empty;
+                mem->unlock();
+                i = bullets.erase(i);
+            }
+        }
+    }
+};
+
+class EvilPlane: public Plane {
+    using Plane::Plane;
+public:
+    virtual void run() {
+        bullets.insert(bullets.begin(), {position.x, position.y});
+        for (QList<node>::iterator i = bullets.begin(); i != bullets.end();) {
+            if (i->x != 24) {
+                ++i->x;
+                mem->lock();
+                if (memdata[i->x - 1][i->y] == EvilBulletT) memdata[i->x - 1][i->y] = Empty;
+                if (memdata[i->x][i->y] == EvilPlaneT) setStatus(Victory);
+                else if (memdata[i->x][i->y == MyPlaneT]) setStatus(Failure);
+                else memdata[i->x][i->y] = EvilBulletT;
+                mem->unlock();
+                ++i;
+            } else {
+                mem->lock();
+                memdata[i->x][i->y] = Empty;
+                mem->unlock();
+                i = bullets.erase(i);
+            }
+        }
+    }
+};
 
 class Widget : public QWidget {
     Q_OBJECT
 
-    QLabel *myplane, *yourplane;
-    QThread myp_thread, yourp_thread;
-    QTimer *m_clock_intr_device, *m_draw_intr_device;
-    QRandomGenerator random;
-    int mem[25][80];
-    QReadWriteLock memLock;
+    QTimer *m_clock_intr_device;
+
+    QSharedMemory *mem;
+    nodeType (*memdata)[ELEMENT_WIDTH_NUM];
+    QImage *myPlaneImage, *evilPlaneImage;
+
     QPainter painter;
-    virtual void keyPressEvent(QKeyEvent *e);
+    QRandomGenerator random;
+
+    MyPlane *myp;
+    EvilPlane *evilp;
+
+    statusType m_status;
+
+    void ClockIntrExec();
+    void handleStatus(statusType status);
+
     virtual void paintEvent(QPaintEvent *e);
 public:
     Widget(QWidget *parent = nullptr);
-    void ClockIntrExec();
-    void KeyIntrExec();
     ~Widget();
 };
 #endif // WIDGET_H
